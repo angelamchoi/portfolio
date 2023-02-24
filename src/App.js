dc123e77c56a8d9479d46c303f9e888edc2cf7ca
Angela Choi <angelamchoi@gmail.com>
2023-02-24 12:36:11 -0500

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AboutUs from "./pages/AboutMe";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+min-height: 10vh;
+display: flex;
+margin: auto;
+justify-content: space-between;
+align-items: center;
+padding: 1rem 10rem;
+background-color: 282828;
+a {
+  color: white;
+  text-decoration: none;
+}
+ul {
+  display: flex;
+  list-style: none;
+}
+h1 {
+  font-size: 1.8rem;
+  /* font-weight: lighter; */
+}
+li {
+  padding-left: 10rem;
+  position: relative;
+}
+`
+
+const Nav = () => {
+  return(
+    <StyledNav>
+      <h1><a href="#">Angela Choi</a></h1>
+      <ul>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">My Work</a></li>
+        <li><a href="#">Contact Me</a></li>
+      </ul>
+    </StyledNav>
+  )
+}
+
+export default Nav;
+
