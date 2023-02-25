15db7fe56a66ce9b493f4e36ddb365cc604e865a
Angela Choi <angelamchoi@gmail.com>
2023-02-24 22:44:07 -0500

    removed color animation from about me and contact

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,27 +4,6 @@ import AboutSection from "../components/AboutSection";
 import { motion } from "framer-motion";
 
 //Color Animation
-const Frame1 = styled(motion.div)`
-  position: fixed;
-  left: 0;
-  top: 10%; 
-  width: 100%;
-  height: 100vh;
-  background: #00FFFF;
-  z-index: 2; /*covers the whole screen*/ 
-`;
-const Frame2 = styled(Frame1)`
-  background: #1E90FF;
-`;
-const Frame3 = styled(Frame1)`
-  background: #0066b2;
-`;
-const Frame4 = styled(Frame1)`
-  background: #72A0C1;
-`;
-const Frame5 = styled(Frame1)`
-background: #002D62;
-`;
 
 const StyledAboutMe = styled(motion.div)`
   min-height: 100vh;
@@ -45,14 +24,7 @@ const Home = () => {
       variants={pageAnimation} 
       initial="hidden" 
       animate="show">
-        <motion.div variants={sliderContainer}>
-        <Frame1 variants={slider}></Frame1>
-        <Frame2 variants={slider}></Frame2>
-        <Frame3 variants={slider}></Frame3>
-        <Frame4 variants={slider}></Frame4>
-        <Frame5 variants={slider}></Frame5>
-      </motion.div>
-   <AboutSection/>
+      <AboutSection/>
     </StyledAboutMe>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,29 +3,6 @@ import styled from "styled-components";
 import { pageAnimation, slider, sliderContainer } from "../animation";
 import { motion } from "framer-motion";
 
-//Color Animation
-const Frame1 = styled(motion.div)`
-  position: fixed;
-  left: 0;
-  top: 10%; 
-  width: 100%;
-  height: 100vh;
-  background: #D8BFD8;
-  z-index: 2; /*covers the whole screen*/ 
-`;
-const Frame2 = styled(Frame1)`
-  background: #DDA0DD;
-`;
-const Frame3 = styled(Frame1)`
-  background: #662d91;
-`;
-const Frame4 = styled(Frame1)`
-  background: #452c63;
-`;
-const Frame5 = styled(Frame1)`
-background: #33006F;
-`;
-
 const StyledContact = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
@@ -46,16 +23,9 @@ const Contact = () => {
       initial="hidden"
       animate="show"
 >
-    <motion.div variants={sliderContainer}>
-      <Frame1 variants={slider}></Frame1>
-      <Frame2 variants={slider}></Frame2>
-      <Frame3 variants={slider}></Frame3>
-      <Frame4 variants={slider}></Frame4>
-      <Frame5 variants={slider}></Frame5>
-    </motion.div>
-<ContactSection/>
-</StyledContact>
- );
+      <ContactSection/>
+    </StyledContact>
+  );
 }
 
 
