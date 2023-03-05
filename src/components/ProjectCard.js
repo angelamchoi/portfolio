307d79674087a2cd1626c3d01fa74ed52e79548d
Angela Choi <angelamchoi@gmail.com>
2023-03-04 19:54:53 -0500

    fixed styling in projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -12,48 +12,58 @@ const StyledProjectCard = styled.div`
   margin-right: 35px;
   margin-bottom: 35px;
   border: 2px solid #23d997;
-  height: 360px;
   width: 360px;
   margin-left: 10px;
   `;
 
 const StyledLink = styled.a`
-color: #34c6eb;
-font-size: 12px ;
+  color: #34c6eb;
+  font-size: 12px ;
 `;
 
 const ButtonActions = styled.div`
-margin-top: -28px;
-padding-left: 10px;
+  margin-top: -28px;
+  padding-left: 10px;
+`;
+
+const StyledDescription = styled.div`
+  padding: 3rem 0rem;
+  font-size: 1.2rem;
+  line-height: 1.43;
+  letter-spacing: 0.01071em;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
+  margin: 0px;
 `;
 
 const StyledTechnologiesUsed = styled.div`
-color: #ccc;
-font-weight: 400;
-font-size: 0.875rem;
-line-height: 1.43;
-letter-spacing: 0.01071em;
-font-family: "Roboto","Helvetica","Arial",sans-serif;
-margin: 0px;
-padding-bottom: 35px;
+  color: #ccc;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.43;
+  letter-spacing: 0.01071em;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
+  margin: 0px;
 `;
 
 const StyledTechnolgiesUsedMargin = styled.div`
-margin-top: -25px;
+  margin-top: -5px;
 `;
 
 const StyledTechnolgiesUsedLessMargin = styled.div`
-margin-top: -20px;
+  margin-bottom: 10px;
 `;
 
 const StyledTechnolgiesUsedMorePadding = styled.div`
-padding-bottom: 35px;
+  margin-bottom: 25px;
 `
 
-const StyledTechnolgiesUsedLessPadding = styled.div`
-margin-bottom: -15px;
-`
+const CardGameStyledTechnologiesUsed = styled.div`
+  padding-bottom: 75px;
+  `;
 
+const GameBreakTechnolgiesUsedPadding = styled.div`
+  padding-bottom: 65px;
+`
 
 const ProjectCard = () => {
   return (
@@ -67,13 +77,13 @@ const ProjectCard = () => {
         <Typography gutterBottom variant="h4" component="div" color="white">
         🃏 Card Game
         </Typography>
-        <Typography variant="body2" color="#ccc;">
+        <StyledDescription>
         Matching card game is a browser based game with a multiplication twist. 
-        </Typography>
+        </StyledDescription>
         <StyledTechnologiesUsed>
           <span>Technologies Used:</span> HTML, CSS, JavaScript
         </StyledTechnologiesUsed>
-        <StyledTechnolgiesUsedMorePadding/>
+        <CardGameStyledTechnologiesUsed/>
       </CardContent>
         <ButtonActions>
           <Button size="small" a href="https://github.com/angelamchoi/card-game" target="_blank"><StyledLink>💻Code</StyledLink></Button>
@@ -89,15 +99,16 @@ const ProjectCard = () => {
         <Typography gutterBottom variant="h4" component="div" color="white">
         📝 Plutonotes
         </Typography>
-        <Typography variant="body2" color="#ccc;">
-        Plutonotes is a web application that allows you to create notes and post your notes publicly for anyone to see. 
-        </Typography>
+        <StyledDescription>
+          Plutonotes is a web application that allows you to create notes and post your notes publicly for anyone to see. 
+        </StyledDescription>
         <StyledTechnologiesUsed>
         <StyledTechnolgiesUsedMargin>
           <span>Technologies Used:</span> HTML, CSS, JavaScript, Node.js, Express, MongoDB, Mongoose, Materialize CSS, Oauth, Passport
           </StyledTechnolgiesUsedMargin>
         </StyledTechnologiesUsed>
       </CardContent>
+      <StyledTechnolgiesUsedMorePadding/>
       <ButtonActions>
         <Button size="small" a href="https://github.com/angelamchoi/plutonotes" target="_blank"><StyledLink>💻Code</StyledLink></Button>
       </ButtonActions>
@@ -111,12 +122,13 @@ const ProjectCard = () => {
         <Typography gutterBottom variant="h4" component="div" color="white">
         🕹️ gamebreak
         </Typography>
-        <Typography variant="body2" color="#ccc">
+        <StyledDescription>
         gamebreak is a web application that allows you to discover and track your video games. 
-        </Typography>
+        </StyledDescription>
         <StyledTechnologiesUsed>
           <span>Technologies Used:</span> HTML, CSS, Python, PostgreSQL, Materialize CSS, Authentication, AWS S3 Storage
-        </StyledTechnologiesUsed>    
+        </StyledTechnologiesUsed>  
+        <GameBreakTechnolgiesUsedPadding/>
        </CardContent>
       <ButtonActions>
         <Button size="small" a href="https://github.com/angelamchoi/gamebreak" target="_blank"><StyledLink>💻Code</StyledLink></Button>
@@ -131,15 +143,15 @@ const ProjectCard = () => {
         <Typography gutterBottom variant="h4" component="div" color="white">
         🔮 quizzify
         </Typography>
-        <Typography variant="body2" color="#ccc;">
+        <StyledDescription>
         quizzify is a web application that allows you to add trivia questions and play trivia games to enhance your trivia gaming skills. 
-        </Typography>
+        </StyledDescription>
         <StyledTechnologiesUsed>
           <StyledTechnolgiesUsedLessMargin>
           <span>Technologies Used:</span> HTML, CSS, React.js, Node.js, Express,Bootstrap, MongoDB, Mongoose, Token-based authentication, AJAX, RESTful routing
           </StyledTechnolgiesUsedLessMargin>
         </StyledTechnologiesUsed>
-        <StyledTechnolgiesUsedLessPadding/>
+        <StyledTechnolgiesUsedMorePadding/>
       </CardContent>
       <ButtonActions>
         <Button size="small" a href="https://github.com/angelamchoi/react-trivia-game" target="_blank"><StyledLink>💻Code</StyledLink></Button>
